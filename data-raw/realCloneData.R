591c3364983e5b40fa842c41da5a8597b08a6a12
bdj34 <brian.d.johnson97@gmail.com>
2023-02-08 07:41:07 -0800

    rename data documentation file

--- data-raw/realCloneData.R
+++ data-raw/realCloneData.R
@@ -2,5 +2,10 @@
 
 # For now, just load in the clone list
 realCloneData <- readRDS("~/rotation_fall2021/ultrametric_trees_with_clades_as_labeled_all_FOUR_papers_12192022.rds")
+setwd("~/Downloads")
+system("git clone https://github.com/margaretefabre/Clonal_dynamics")
+setwd("Clonal_dynamics/Phylogenies/Files")
+system("ls")
+
 
 usethis::use_data(realCloneData, overwrite = TRUE)
