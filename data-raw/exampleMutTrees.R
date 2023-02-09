7d65cf1ad6bfbd618abb76b9e083d8f5035b51c3
bdj34 <brian.d.johnson97@gmail.com>
2023-02-09 11:37:52 -0800

    update data for example simulated trees

--- data-raw/exampleMutTrees.R
+++ data-raw/exampleMutTrees.R
@@ -11,11 +11,18 @@ for (i in c(1:100)){
   cloneYrs <- 20
   n_tips = 100
   tree <- cloneRate::simTree(a, b, cloneAge = cloneYrs, n=n_tips)
-  tree$params <- data.frame(r=1, "a" = a, "b" = b, "cloneAge" = cloneYrs, "n" = n_tips)
+  nu <- stats::runif(1, 10, 20)
+
+  for(i in c(1:length(tree$edge.length))){
+    tree$edge.length[i] <- stats::rpois(1, nu*tree$edge.length[i])
+  }
+
+  tree$params <- data.frame(r=1, "a" = a, "b" = b, "cloneAge" = cloneYrs, "n" = n_tips, "nu" = nu)
   tree_list <- append(tree_list, list(tree))
 
 }
 
 # Rename object and save
-exampleUltraTrees <- tree_list
-usethis::use_data(exampleUltraTrees, overwrite = TRUE, compress = "bzip2")
+exampleMutTrees <- tree_list
+usethis::use_data(exampleMutTrees, overwrite = TRUE, compress = "bzip2")
+
