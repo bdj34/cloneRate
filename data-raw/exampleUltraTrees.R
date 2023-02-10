be325db078a3bc0afff4c0c941139b9812b658a4
bdj34 <brian.d.johnson97@gmail.com>
2023-02-09 18:49:29 -0800

    styler edits to exampleUltraTrees.R

--- data-raw/exampleUltraTrees.R
+++ data-raw/exampleUltraTrees.R
@@ -4,15 +4,14 @@
 tree_list <- list()
 
 # Generate 100 trees, appending to list
-for (i in c(1:100)){
+for (i in c(1:100)) {
   a <- stats::runif(1, min = 1, max = 2)
-  b <- a-1
+  b <- a - 1
   cloneYrs <- 20
-  n_tips = 100
-  tree <- cloneRate::simTree(a, b, cloneAge = cloneYrs, n=n_tips)
-  tree$params <- data.frame(r=1, "a" = a, "b" = b, "cloneAge" = cloneYrs, "n" = n_tips)
+  n_tips <- 100
+  tree <- cloneRate::simTree(a, b, cloneAge = cloneYrs, n = n_tips)
+  tree$params <- data.frame(r = 1, "a" = a, "b" = b, "cloneAge" = cloneYrs, "n" = n_tips)
   tree_list <- append(tree_list, list(tree))
-
 }
 
 # Rename object and save
