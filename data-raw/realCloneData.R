3d424b1eff693d217565dadb4fedeb1a1b24b85e
bdj34 <brian.d.johnson97@gmail.com>
2023-02-10 07:49:57 -0800

    remove header from realCloneData.R with rds file loading data

--- data-raw/realCloneData.R
+++ data-raw/realCloneData.R
@@ -1,9 +1,5 @@
 ## code to prepare `realCloneData` dataset goes here
 
-####### Following to be removed
-# For now, just load in the clone list
-# realCloneData <- readRDS("~/rotation_fall2021/ultrametric_trees_with_clades_as_labeled_all_FOUR_papers_12192022.rds")
-#######
 rm(list = ls())
 library(ggtree)
 library(phangorn)
