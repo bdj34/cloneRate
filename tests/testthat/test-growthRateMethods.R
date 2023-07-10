16c8db7104217cd2aaabbd03d811b5924d0fa4ed
bdj34 <brian.d.johnson97@gmail.com>
2023-07-10 15:27:17 -0400

    test commit - no change

--- tests/testthat/test-growthRateMethods.R
+++ tests/testthat/test-growthRateMethods.R
@@ -5,6 +5,7 @@ test_that("shared mutations method needs mutation rate", {
   # sharedMuts() will find mutation rate if nu is a col in metadata data.frame
   expect_no_error(sharedMuts(mutTree))
 
+
   # Remove metadata and sharedMuts() should fail
   mutTree$metadata <- NULL
   expect_error(sharedMuts(mutTree), regexp = "Need to give a mutation rate")
